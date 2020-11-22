21fd0dbabeee4b8342b099e21cdb7bbfc1670467
maybejustmaybe <maybenotmaybe@gmail.com>
2020-11-22 17:00:11 -0500

    Refactoring and skeleton for handling inbounds

---
+++ automation/models.py
@@ -0,0 +1,43 @@
+import enum
+from typing import *
+
+import pydantic
+
+from .utils.airtable import MetaBaseModel
+
+
+class InboundModel(MetaBaseModel):
+    method: str = pydantic.Field(alias="Method of Contact")
+    phone_number: Optional[str] = pydantic.Field(alias="Phone Number")
+    message: Optional[str] = pydantic.Field(alias="Message")
+    voicemail_recording: Optional[str] = pydantic.Field(alias="Voicemail Recording")
+    # TODO : intake volunteer shouldn't be one-only
+    intake_volunteer: List[str] = pydantic.Field(
+        default_factory=list, alias="Intake Volunteer"
+    )
+    intake_time: str = pydantic.Field(alias="Intake Time")
+    other_inbounds: List[str] = pydantic.Field(
+        default_factory=list, alias="Other Inbounds"
+    )
+
+    @staticmethod
+    def get_valid_statuses():
+        return {
+            "Intake Needed",
+            "In Progress",
+            "Intake Complete",
+            "Duplicate",
+            "Outside Bed-Stuy",
+            "Call Back",
+            "Question/Info",
+            "Thank you!",
+            "Spanish-Intake needed",
+            "No longer needs assistance",
+            "Phone Tag",
+            "Out of Service/Cannot Reach",
+        }
+
+    @pydantic.validator("method")
+    def validate_method(cls, v):
+        if v not in {"Email", "Phone Call", "Text Message"}:
+            raise ValueError("Invalid method value: {}".format(v))
--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from core import airtable, tables
+import flask
+import twilio
+from twilio.twiml.messaging_response import MessagingResponse
+
+from utils import airtable, tables
 from functions import inbound
+from models import InboundModel
 
-#########
-# UTILS #
-#########
-
-def poll_table(table, status_to_cb):
-    client = airtable.Client()
-    recs = client.poll(table)
-
-    for record in client.poll(table):
-        cb = status_to_cb.get(record.status)
-
-        if cb is not None:
-            cb(record)
 
 ##########################
 # GOOGLE CLOUD FUNCTIONS #
 ##########################
 
+
 def poll_inbounds(event, context):
     print("event: {}".format(event))
     print("context: {}".format(context))
 
-    poll_table(tables.Table.INBOUND, {None: inbound.on_new})
+    poll_table(tables.Table.INBOUND)
+
+
+def handle_inbound_message(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    print(request_json)
+    print(request_args)
+
+    model = InboundModel(
+        method="Text Message",
+        phone_number=request_json["From"],
+        message=request_json["Body"],
+    )
+
+    print(model)
+
+    return str(MessagingResponse())
