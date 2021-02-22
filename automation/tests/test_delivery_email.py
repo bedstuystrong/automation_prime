b5da20bb920f62027fb903b1e1fc4cca6bdda39b
Leif Walsh <leif.walsh@gmail.com>
2021-02-22 07:27:39 +0000

    add more targeted test

--- automation/tests/test_delivery_email.py
+++ automation/tests/test_delivery_email.py
@@ -12,7 +12,10 @@ from hypothesis.strategies import (
     text,
 )
 
-from automation.functions.delivery import construct_delivery_email
+from automation.functions.delivery import (
+    construct_delivery_email,
+    render_email_template,
+)
 from automation.models import IntakeModel, MemberModel
 
 
@@ -53,6 +56,43 @@ def test_construct_delivery_email(data, volunteer_id):
 
     intake_table.get.assert_called_once_with(ticket.id)
     member_table.get.assert_called_once_with(volunteer.id)
+
+    # sendgrid's "to" list is weird, it's inside "personalizations" and has an
+    # odd format
+    to = next(p["to"] for p in email.get()["personalizations"] if "to" in p)
+    to_emails = [r["email"] for r in to if "email" in r]
+    assert (
+        volunteer.email in to_emails
+        or f"{volunteer.name} <{volunteer.email}>" in to_emails
+    )
+    expected_subject = (
+        f"[Bed Stuy Strong] Delivery Instructions for {ticket.ticket_id}"
+    )
+    assert str(email.subject) == expected_subject
+    for food_option in ticket.food_options:
+        assert food_option in email.content
+
+
+@settings(max_examples=1000)
+@given(
+    ticket=builds(
+        IntakeModel,
+        status=just("Assigned / In Progress"),
+    ),
+    volunteer=builds(
+        MemberModel,
+        **{
+            "Name": text(
+                alphabet=characters(
+                    whitelist_categories=("L", "Zs"),
+                )
+            ),
+            "Email Address": emails(),
+        },
+    ),
+)
+def test_render_delivery_email(ticket, volunteer):
+    email = render_email_template(ticket, [volunteer])
 
     # sendgrid's "to" list is weird, it's inside "personalizations" and has an
     # odd format
