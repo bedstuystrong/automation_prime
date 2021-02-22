1b07bc6dbb1d3c19257fec1831dc4f86bc2733d7
Leif Walsh <leif.walsh@gmail.com>
2021-02-22 05:28:13 +0000

    refactor delivery email a bit

--- automation/functions/delivery.py
+++ automation/functions/delivery.py
@@ -1,5 +1,16 @@
-from automation.clients.templates import render
+from requests.exceptions import HTTPError
 from sendgrid.helpers.mail import Mail
+from structlog.contextvars import bind_contextvars
+
+from automation.clients.templates import render
+
+
+class DeliveryEmailError(Exception):
+    """Error constructing delivery email."""
+
+    def __init__(self, msg, status_code):
+        self.msg = msg
+        self.status_code = status_code
 
 
 def check_ready_to_send(ticket):
@@ -23,3 +34,38 @@ def render_email_template(ticket, delivery_volunteers):
         ),
     )
     return message
+
+
+def construct_delivery_email(log, request, intake_table, member_table):
+    log.info("Sending delivery email", args=dict(request.args))
+    try:
+        record_id = request.args["record_id"]
+    except KeyError:
+        msg = "Must provide record_id"
+        log.error(msg)
+        raise DeliveryEmailError(msg, 400)
+    try:
+        ticket = intake_table.get(record_id)
+    except HTTPError as e:
+        msg = f"Error finding intake ticket: {e.response.text}"
+        log.error(msg, exc_info=True)
+        raise DeliveryEmailError(msg, e.response.status_code)
+
+    bind_contextvars(ticket_id=ticket.ticket_id)
+
+    problem = check_ready_to_send(ticket)
+    if problem:
+        msg = "Ticket not ready to send"
+        log.error(msg, problem=problem)
+        raise DeliveryEmailError(problem, 412)
+
+    delivery_volunteers = []
+    for r in ticket.delivery_volunteer:
+        try:
+            delivery_volunteers.append(member_table.get(r))
+        except HTTPError as e:
+            msg = f"Error finding delivery volunteer {r}: {e.response.text}"
+            log.error(msg, exc_info=True)
+            raise DeliveryEmailError(msg, e.response.status_code)
+
+    return render_email_template(ticket, delivery_volunteers)
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import logging
 
 from python_http_client.exceptions import BadRequestsError
-from requests.exceptions import HTTPError
 import sendgrid
 import structlog
-from structlog.contextvars import bind_contextvars
 
 from automation import cloud_logging, config, tables  # noqa: E402
-from automation.functions.delivery import (
-    check_ready_to_send,
-    render_email_template,
-)
+from automation.functions import delivery
 
 
 cloud_logging.configure()
@@ -36,35 +31,15 @@ sendgrid_client = sendgrid.SendGridAPIClient(conf.sendgrid.api_key)
 def send_delivery_email(request):
     log = structlog.get_logger("send_delivery_email")
     cloud_logging.bind_trace_id(request, conf.google_cloud.project_id)
-    log.info("Sending delivery email", args=dict(request.args))
     try:
-        record_id = request.args["record_id"]
-    except KeyError:
-        return "Must provide record_id", 400
-    try:
-        ticket = intake_table.get(record_id)
-    except HTTPError as e:
-        msg = f"Error finding intake ticket: {e.response.text}"
-        log.error(msg, exc_info=True)
-        return msg, e.response.status_code
-
-    bind_contextvars(ticket_id=ticket.ticket_id)
-
-    problem = check_ready_to_send(ticket)
-    if problem:
-        log.error("Ticket not ready to send", problem=problem)
-        return problem, 412
-
-    delivery_volunteers = []
-    for r in ticket.delivery_volunteer:
-        try:
-            delivery_volunteers.append(member_table.get(r))
-        except HTTPError as e:
-            msg = f"Error finding delivery volunteer {r}: {e.response.text}"
-            log.error(msg, exc_info=True)
-            return msg, e.response.status_code
-
-    email = render_email_template(ticket, delivery_volunteers)
+        email = delivery.construct_delivery_email(
+            log,
+            request,
+            intake_table,
+            member_table,
+        )
+    except delivery.DeliveryEmailError as e:
+        return e.msg, e.status_code
 
     email.from_email = conf.sendgrid.from_email
     email.add_cc(conf.sendgrid.reply_to)
@@ -77,6 +52,6 @@ def send_delivery_email(request):
         log.error(msg, body=e.body, exc_info=True)
         return msg, e.status_code
 
-    msg = f"Sent email for {ticket.ticket_id}"
+    msg = "Sent email"
     log.info(msg)
     return msg, 200
