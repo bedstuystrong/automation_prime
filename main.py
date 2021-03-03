ee9b3c601a0e930eea9990c1637f5e91cda48e43
Vikas Arun <vikas.arun@gmail.com>
2021-03-03 07:46:19 +0000

    changing main imports

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from slack_logger import SlackHandler, SlackFormatter
 from automation import cloud_logging, config, tables  # noqa: E402
 #Adding a handler to output error messages from GCP to slack
 #Documentation: https://pypi.org/project/slack-logger/
-conf = config.load()
-sh = SlackHandler(username='gcp_error_bot', icon_emoji=':robot_face:', url=conf.slack.error_bot_webhook)
-f = SlackFormatter()
-sh.setFormatter(f)
-logging.basicConfig(level=logging.INFO, handlers=[sh])
->>>>>>> b345a46 (Adding slack logger)
 
+# Adding a handler to output error messages from GCP to slack
+# Documentation: https://pypi.org/project/slack-logger/
 
 cloud_logging.configure()
 conf = config.load()
+sh = SlackHandler(
+    username="gcp_error_bot",
+    icon_emoji=":robot_face:",
+    url=conf.slack.error_bot_webhook,
+)
+f = SlackFormatter()
+sh.setFormatter(f)
+logging.basicConfig(level=logging.INFO, handlers=[sh])
 
 
 ##########################
