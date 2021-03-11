f0e60c786387385aab1448e6bb53cf6a76280819
alex baldwin <ghostbaldwin@gmail.com>
2021-03-11 01:29:31 +0000

    create slack users with scim

--- automation/clients/slack.py
+++ automation/clients/slack.py
@@ -1,9 +1,16 @@
-from automation.config import SlackConfig
 import enum
+import random
+import string
 from typing import Optional
 
 import pydantic
+import requests
+from six.moves import xrange
 import slack_sdk
+from slack_sdk import scim
+from slack_sdk.scim.v1.user import UserEmail
+
+from automation.config import SlackConfig
 
 ##########
 # MODELS #
@@ -74,6 +81,13 @@ class SlackErrors(enum.Enum):
     USERS_NOT_FOUND = "users_not_found"
 
 
+def generate_password(length):
+    charset = string.ascii_letters + string.digits
+    return "".join(
+        [random.SystemRandom().choice(charset) for _ in xrange(length)]
+    )
+
+
 # TODO : add support for other slack client methods
 #
 # TODO : make the interface better and support pagination and
@@ -85,6 +99,9 @@ class SlackErrors(enum.Enum):
 class SlackClient:
     def __init__(self, conf: SlackConfig):
         self._slack_sdk_client = slack_sdk.WebClient(token=conf.api_key)
+        self._slack_scim_client = scim.SCIMClient(token=conf.scim_api_key)
+        self._resend_invite_webhook = conf.resend_invite_webhook
+        self._resend_invite_secret = conf.resend_invite_secret
 
     def _slack_sdk_wrapper(
         self, slack_sdk_func_name, model_type, data_key, single_result=False
@@ -103,6 +120,27 @@ class SlackClient:
 
         return wrapper
 
+    def _create_scim_user(self, email, name):
+        user = scim.User(
+            user_name=name,
+            display_name=name,
+            active=True,
+            password=generate_password(32),
+            emails=[UserEmail(value=email, primary=True)],
+        )
+        create_result = self._slack_scim_client.create_user(user)
+        return create_result.user
+
+    def _resend_invite(self, email):
+        headers = {
+            "Authorization": "Bearer %s" % self._resend_invite_secret,
+        }
+        res = requests.post(
+            self._resend_invite_webhook, headers=headers, json={"email": email}
+        )
+        res.raise_for_status()
+        return res.text
+
     def users_lookupByEmail(self, email):
         try:
             return self._slack_sdk_wrapper(
@@ -116,3 +154,14 @@ class SlackClient:
                 return None
             else:
                 raise
+
+    def users_invite(self, email, name):
+        user = self._create_scim_user(email, name)
+        self._resend_invite(email)
+        return User(
+            id=user.id,
+            name=name,
+            profile=Profile(
+                email=email,
+            ),
+        )
--- automation/functions/members.py
+++ automation/functions/members.py
@@ -1,6 +1,6 @@
 import logging
 
-import sendgrid.helpers.mail
+from sendgrid.helpers.mail import Mail
 
 from ..utils import templates
 
@@ -8,24 +8,21 @@ logger = logging.getLogger(__name__)
 
 
 def on_new(member, *, slack_client, sendgrid_client, auth0_client, from_email):
-    # TODO : the member may not have joined slack yet, so we should
-    # try and update their slack info again later
-
-    # Lookup and store the member's slack metadata
+    # Look up and store the member's slack metadata
     slack_user = slack_client.users_lookupByEmail(member.email)
+    if slack_user is None:
+        slack_user = slack_client.users_invite(member.email, member.name)
 
-    if slack_user is not None:
-        member.slack_handle = slack_user.get_handle()
-        member.slack_email = slack_user.profile.email
-        member.slack_user_id = slack_user.id
-    else:
-        logger.warning("Couldn't find slack user for: {}".format(member.email))
+    member.slack_handle = slack_user.get_handle()
+    member.slack_email = slack_user.profile.email
+    member.slack_user_id = slack_user.id
 
+    # Create Auth0 user for Member Hub
     auth0_client.create_user(member.email, member.name)
 
     # Send the new member email
     sendgrid_client.send(
-        sendgrid.helpers.mail.Mail(
+        Mail(
             from_email=from_email,
             to_emails=member.email,
             subject="Welcome to Bed-Stuy Strong {}!".format(member.name),
