ce6f0c03b5b714175fa29abf992db9aa6e18b355
alex baldwin <ghostbaldwin@gmail.com>
2021-03-31 02:19:17 +0000

    remove slack email test

--- automation/tests/test_members.py
+++ automation/tests/test_members.py
@@ -45,7 +45,6 @@ def test_on_new():
         from_email="test@example.org",
     )
 
-    assert test_member.slack_email == test_slack_user.profile.email
     assert test_member.slack_user_id == test_slack_user.id
 
     assert mock_sendgrid_client.send.call_count == 1
